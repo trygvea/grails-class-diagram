dcf8fafb0c7a21b1741ea27845f5b89bd77f0fbb
trygvea <trygvea@832c1c66-4827-0410-8465-ccb17913cd09>
2009-10-22 09:57:05 +0000

    upgrade to grails-1.2-M3
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-class-diagram/trunk@56700 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -20,3 +20,7 @@ log4j = {
     debug    'DotBuilder' // root
 
 }
+
+// The following properties have been added by the Upgrade process...
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
