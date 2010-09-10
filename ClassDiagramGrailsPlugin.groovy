599e0e06dcaec0e58b399e96274abbea2f9cd8d4
trygvea <trygve.amundsen@gmail.com>
2010-09-10 23:33:26 +0200

    Upgraded to grails 1.3.4

--- ClassDiagramGrailsPlugin.groovy
+++ ClassDiagramGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class ClassDiagramGrailsPlugin {
     // the plugin version
     def version = "0.5.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.0 > *"
+    def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
